fb1f20c588ee17a6506e60ca0394aa4c5fc35286
test2 <test2@Zdeneks-Laptop.local>
2024-11-23 00:13:10 +0100

    script update

--- aoc.py
+++ aoc.py
@@ -25,7 +25,8 @@ config_parser.add_argument(
 
 init_parser = subparsers.add_parser("init", help="Initialize solution")
 init_parser.add_argument(
-    "-d", nargs="+", metavar=('day', 'year'), required=False, help="Day, year of solution")
+    "year", type=int, help="The year of the advent of code")
+init_parser.add_argument("day", type=int, help="The day of the advent of code")
 init_parser.add_argument("-t", "--template", required=False,
                          help="Path to template")
 init_parser.add_argument(
@@ -39,7 +40,25 @@ submit_parser.add_argument(
     "day", type=int, help="The day of the advent of code")
 submit_parser.add_argument("level", type=int, help="The level of the solution")
 submit_parser.add_argument(
-    "-s", required=False, help="The value of the solution")
+    "-s", required=False, type=int, help="The value of the solution")
+
+
+def __init_solution(year, day, template_dir, input_dir, sessid):
+    os.makedirs(f"{year}/{day}", exist_ok=True)
+
+    # copy template content to the year/day directory
+    if template_dir and os.path.exists(template_dir):
+        if os.path.isdir(template_dir):
+            shutil.copytree(template_dir, f"{year}/{day}", dirs_exist_ok=True)
+        elif os.path.isfile(template_dir):
+            # execute the file inside the year/day directory
+            print("Not supported yet.")
+            sys.exit(3)
+
+    # fetch input into provided directory
+    if input_dir:
+        os.makedirs(f"{year}/{day}/{input_dir}", exist_ok=True)
+        __fetch_input(year, day, sessid, input_dir)
 
 
 def __submit_solution(year, day, level, value, session):
@@ -49,6 +68,7 @@ def __submit_solution(year, day, level, value, session):
     curl_cmd = f"curl -X POST -H 'Cookie: session={session}' " + \
         f"-d 'level={level}&answer={value}' " + \
         f"https://adventofcode.com/{year_full}/day/{day}/answer"
+    print(curl_cmd)
     return os.system(curl_cmd)
 
 
@@ -67,56 +87,29 @@ if __name__ == "__main__":
 
     if parsed_args.command == 'config':
         opt, value = parsed_args.opt, parsed_args.value
-        print(f"setting <{opt}> to \"{value}\"")
+        print(f"Set <{opt}> to \"{value}\"")
         with open(CONFIG_FILE, "w") as f:
             f.write(f"{opt}={value}")
+        sys.exit(0)
         pass
 
-    elif parsed_args.command == 'init':
-        dir = parsed_args.d
-        print(f"Initializing solution dir for {dir}")
-        if len(dir) == 2:
-            # create directory for the solution for cwd
-            year, day = dir
-        input = parsed_args.input
+    sessid = None
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE) as f:
+            sessid = f.read().strip().split("=")[1]
+
+    if parsed_args.command == 'init':
+        year, day, template_dir, input_dir = parsed_args.year, parsed_args.day, parsed_args.template, parsed_args.input
+        if input_dir and not sessid:
+            print("Session cookie not set. Run `aoc config sessid <value>`")
+            sys.exit(2)
+        __init_solution(year, day, template_dir, input_dir, sessid)
+    elif parsed_args.command == 'submit':
+        year, day, level, value = parsed_args.year, parsed_args.day, parsed_args.level, parsed_args.s
+        if not sessid:
+            print("Session cookie not set. Run `aoc config sessid <value>`")
+            sys.exit(2)
+        __submit_solution(year, day, level, value, sessid)
     else:
         print("No arguments provided. Run with -h for help.")
         sys.exit(1)
-
-        # year, day = parsed_args.year, parsed_args.day
-        # template_dir = parsed_args.template
-        # input_dir = parsed_args.input
-        # submit = parsed_args.submit
-        # cookie = parsed_args.cookie
-        #
-        # os.makedirs(f"{year}/{day}", exist_ok=True)
-        #
-        # SSID = None
-        # ssid_file = SESSION_COOKIE
-        # if input_dir or submit:
-        #     if cookie:
-        #         if os.path.exists(cookie) and os.path.isfile(cookie):
-        #             ssid_file = cookie
-        #         else:
-        #             SSID = cookie
-        #     if SSID is None and os.path.exists(ssid_file):
-        #         with open(ssid_file) as f:
-        #             SSID = f.read().strip()
-        #     print(f"Session cookie: {SSID}\n")
-        #
-        # # copy template content to the year/day directory
-        # if template_dir and os.path.exists(template_dir):
-        #     if os.path.isdir(template_dir):
-        #         shutil.copytree(template_dir, f"{year}/{day}", dirs_exist_ok=True)
-        #     elif os.path.isfile(template_dir):
-        #         # execute the file inside the year/day directory
-        #         print("Not supported yet.")
-        #         sys.exit(3)
-        #
-        # # fetch input into provided directory
-        # if input_dir:
-        #     os.makedirs(f"{year}/{day}/{input_dir}", exist_ok=True)
-        #     __fetch_input(year, day, SSID, input_dir)
-        #
-        # if submit:
-        #     __submit_solution(year, day, *submit, SSID)
